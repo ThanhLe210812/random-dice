48dc62efe3afd2903663ab509558cbac01a158d2
ThanhLe210812 <32350845+ThanhLe210812@users.noreply.github.com>
2025-10-14 13:44:31 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function rollDice() {
   const number = Math.floor(Math.random() * 6) + 1;
   document.getElementById("result").textContent = `🎉 Bạn quay được số: ${number}`;
+  
+  // Khóa nút sau khi quay
+  document.querySelector("button").disabled = true;
 }
